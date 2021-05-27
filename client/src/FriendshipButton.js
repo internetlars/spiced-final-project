c176003c85785c803dd8ebe63ab5f15820717e26
Lars Braun <braun.lars@icloud.com>
2021-05-27 10:19:24 +0200

    debug update

--- client/src/FriendshipButton.js
+++ client/src/FriendshipButton.js
@@ -1,16 +1,7 @@
-//let's import the hooks that we will need
 import { useState, useEffect } from "react";
 import axios from "./axios";
 
 export default function FriendshipButton({ id }) {
-    //will be rednered inside other profile
-    //other profile knows the id of the user that is shown, so we need to pass that info down to the friendshipbutton component via props
-    // other profile knows the id from sth like this this.props.match.params.id
-    //in useEffect: make request to the server to find out relation between who is logged in and the profile we are viewing
-    //based on that, we want to set the value of our button text
-    //on clicking of that button, we want to inform the server that there was an update in relation of two users; the one logged in and the one being viewed.
-
-    // text of button should be in state
     const [buttonText, setButtonText] = useState("");
 
     useEffect(() => {
@@ -37,7 +28,7 @@ export default function FriendshipButton({ id }) {
             });
             setButtonText(data.dynamicButtonText);
         } catch (error) {
-            console.log("Error caught in handleSumit: ", error);
+            console.log("Error caught in handleSubmit: ", error);
         }
     };
 
@@ -50,15 +41,13 @@ export default function FriendshipButton({ id }) {
             });
             setButtonText("Add as a friend");
         } catch (error) {
-            console.log("Error in Decline: ", error);
+            console.log("Error in declining: ", error);
         }
     };
 
     return (
         <>
             <button onClick={handleSubmit} className="btn">
-                {/* button will be dynamically set */}
-                {/* OOO */}
                 {buttonText}
             </button>
             {buttonText === "Accept" && (
